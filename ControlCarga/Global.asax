7ad2f07f66af792c75b7ca24eb609f7fd51a2356
Administrator <Administrator@WIN-EBB2JQ4N2TU>
2017-01-20 09:00:13 -0400

    Add project files.

---
+++ ControlCarga/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ControlCarga.MvcApplication" Language="C#" %>
---
+++ ControlCarga/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ControlCarga.Startup))]
+namespace ControlCarga
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
